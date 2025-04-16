0c017f1eba25460516ae55bdffc49d606bb2fe4f
Andrey Arutiunian <andreylzmw@gmail.com>
2025-04-16 15:11:15 +0300

    add 4:1:1 test

--- tests/jpeg_tests.cpp
+++ tests/jpeg_tests.cpp
@@ -105,3 +105,59 @@ TEST(test_jpeg, test_ppm_ycbcr_subsampling_blocks) {
 
 	ppm_file.close();
 }
+
+TEST(test_jpeg, test_ppm_ycbcr_subsampling_blocks_411) {
+	std::filesystem::path resDir(RESOURCE_DIR);
+	std::ifstream ppm_file(resDir / "8x4.ppm");
+	EXPECT_TRUE(ppm_file.is_open());
+
+	PPMImage ppm_image = read_ppm_image(ppm_file);
+	EXPECT_EQ(ppm_image.magicNumber, "P6");
+	EXPECT_EQ(ppm_image.width, 8);
+	EXPECT_EQ(ppm_image.height, 4);
+	EXPECT_EQ(ppm_image.maxVal, 255);
+
+	RawImageData ycbcr_data = rgb_to_ycbcr(ppm_image);
+
+	EXPECT_EQ(ycbcr_data[0], RawChannelData(8, 4, {
+		29, 29, 150, 150, 76, 76, 226, 226,
+		29, 29, 150, 150, 76, 76, 226, 226,
+		179, 179, 255, 255, 0, 0, 105, 105,
+		179, 179, 255, 255, 0, 0, 105, 105
+	}));
+
+	EXPECT_EQ(ycbcr_data[1], RawChannelData(8, 4, {
+		255, 255, 44, 44, 85, 85, 0, 0,
+		255, 255, 44, 44, 85, 85, 0, 0,
+		171, 171, 128, 128, 128, 128, 212, 212,
+		171, 171, 128, 128, 128, 128, 212, 212
+
+	}));
+
+	EXPECT_EQ(ycbcr_data[2], RawChannelData(8, 4, {
+		107, 107, 21, 21, 255, 255, 149, 149,
+		107, 107, 21, 21, 255, 255, 149, 149,
+		0, 0, 128, 128, 128, 128, 235, 235,
+		0, 0, 128, 128, 128, 128, 235, 235
+	}));
+
+	RawImageData subsampled_ycbcr_data = subsampling(ycbcr_data, SubsamplingMode(4, 1, 1));
+
+	EXPECT_EQ(subsampled_ycbcr_data[0], ycbcr_data[0]);
+
+	EXPECT_EQ(subsampled_ycbcr_data[1], RawChannelData(2, 4, {
+		255, 85,
+		255, 85,
+		171, 128,
+		171, 128
+	}));
+
+	EXPECT_EQ(subsampled_ycbcr_data[2], RawChannelData(2, 4, {
+		107, 255,
+		107, 255,
+		0, 128,
+		0, 128,
+	}));
+
+	ppm_file.close();
+}

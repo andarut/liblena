be9b2f1b729585d5cb17d585418a705d8fe12cb4
Andrey Arutiunian <andreylzmw@gmail.com>
2025-05-05 13:05:55 +0300

    huffman codes for DCs

---
+++ include/huffmancoding.h
@@ -0,0 +1,93 @@
+#ifndef HUFFMAN_CODING_H
+#define HUFFMAN_CODING_H
+
+#include "utils.h"
+#include "types.h"
+#include "globals.h"
+
+struct HuffEntry {
+	u16 code;
+	u8  length;
+};
+
+// Table K.3 - Table for luminance DC coefficient differences
+static constexpr std::array<HuffEntry,12> LUMINANCE_DC = {{
+	/*  0 */ { 2, 0b00 },
+	/*  1 */ { 3, 0b010 },
+	/*  2 */ { 3, 0b011 },
+	/*  3 */ { 3, 0b100 },
+	/*  4 */ { 3, 0b101 },
+	/*  5 */ { 3, 0b110 },
+	/*  6 */ { 4, 0b1110 },
+	/*  7 */ { 5, 0b11110 },
+	/*  8 */ { 6, 0b111110 },
+	/*  9 */ { 7, 0b1111110 },
+	/* 10 */ { 8, 0b11111110 },
+	/* 11 */ { 9, 0b111111110 },
+}};
+
+// Table K.4 - Table for chrominance DC coefficient differences
+static constexpr std::array<HuffEntry,12> CHROMINANCE_DC = {{
+	/*  0 */ {  2, 0b00 },
+	/*  1 */ {  2, 0b01 },
+	/*  2 */ {  2, 0b10 },
+	/*  3 */ {  3, 0b110 },
+	/*  4 */ {  4, 0b1110 },
+	/*  5 */ {  5, 0b11110 },
+	/*  6 */ {  6, 0b111110 },
+	/*  7 */ {  7, 0b1111110 },
+	/*  8 */ {  8, 0b11111110 },
+	/*  9 */ {  9, 0b111111110 },
+	/* 10 */ { 10, 0b1111111110 },
+	/* 11 */ { 11, 0b11111111110 },
+}};
+
+/* Table K.5 - Table for luminance AC coefficients */
+static constexpr std::array<HuffEntry, 16*16> LUMINANCE_AC = [](){
+    std::array<HuffEntry,256> tbl{};
+
+    auto set = [&](int run, int size, const char* bits){
+        uint16_t code = 0;
+        uint8_t  len  = 0;
+        for (const char* p = bits; *p; ++p, ++len) {
+            code = (code << 1) | (*p - '0');
+        }
+        tbl[(run<<4) | size] = { code, len };
+    };
+
+    /* (sheet 1 of 4 */
+
+	/* (sheet 2 of 4) */
+
+	/* (sheet 3 of 4) */
+
+	/* (sheet 4 of 4) */
+	
+    return tbl;
+}();
+
+/* Table K.6 - Table for chrominane AC coefficients */
+static constexpr std::array<HuffEntry, 16*16> CHROMINANCE_AC = [](){
+    std::array<HuffEntry,256> tbl{};
+
+    auto set = [&](int run, int size, const char* bits){
+        uint16_t code = 0;
+        uint8_t  len  = 0;
+        for (const char* p = bits; *p; ++p, ++len) {
+            code = (code << 1) | (*p - '0');
+        }
+        tbl[(run<<4) | size] = { code, len };
+    };
+
+	/* (sheet 1 of 4) */
+
+	/* (sheet 2 of 4) */
+
+	/* (sheet 3 of 4) */
+
+	/* (sheet 4 of 4) */
+
+}();
+
+
+#endif // HUFFMAN_CODING_H

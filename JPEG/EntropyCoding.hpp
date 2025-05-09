75b699b32b390d3ed6833bbb6621cace789e7258
Andrey Arutiunian <andreylzmw@gmail.com>
2025-05-09 18:40:55 +0300

    working zigzag test

--- JPEG/EntropyCoding.hpp
+++ JPEG/EntropyCoding.hpp
@@ -1,9 +1,55 @@
-#ifndef HUFFMAN_CODING_H
-#define HUFFMAN_CODING_H
+#ifndef ENTROPYCODING_H
+#define ENTROPYCODING_H
 
-#include "utils.hpp"
-#include "types.hpp"
-#include "globals.h"
+#include "Utils.hpp"
+#include "Types.hpp"
+#include "Logger.hpp"
+#include "Globals.hpp"
+
+template<typename T>
+std::vector<T> zigzag(const ImageChannel<T> data) {
+    std::vector<T> zigzag(data.size());
+
+    u64 read_i = 0, read_j = 0;
+    for (u64 i = 0; i < zigzag.size(); i++) {
+
+        zigzag[i] = data(read_i, read_j);
+
+        if ((read_i+read_j) % 2 == 0) { 
+            if (read_i > 0) read_i--; 
+            if (read_j < data.width()-1) read_j++; 
+        } else { 
+            if (read_i < data.height()-1) read_i++; 
+            if (read_j > 0) read_j--; 
+        }
+    }
+
+    return zigzag;
+}
+
+/* NOTE: this is jpeg's RLE (rely on many zeros in data and omitting DC) */
+template<typename T>
+std::vector<T> RLE(const std::vector<T> data) {
+    std::vector<T> RLE_data;
+    RLE_data.reserve(data.size());
+
+    u64 count = 0;
+    for (u64 i = 1; i < data.size(); i++) {
+        if (data[i] == 0) count++;
+        else {
+            RLE_data.push_back(count);
+            RLE_data.push_back(log2(std::abs(data[i]))+1);
+            RLE_data.push_back(data[i]);
+            count = 0;
+        }
+    }
+
+    // EOB
+    RLE_data.push_back(0);
+    RLE_data.push_back(0);
+
+    return RLE_data;
+}
 
 struct HuffEntry {
 	u8  length;
@@ -468,4 +514,4 @@ void writeChrominanceAC(BitStream &bs, u32 run, u16 size, s32 amplitude) {
 	writeVLI(bs, amplitude, size);
 }
 
-#endif // HUFFMAN_CODING_H
+#endif // ENTROPYCODING_H
